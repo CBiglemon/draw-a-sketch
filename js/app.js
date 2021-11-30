a5e668ef529ebefd90abb33bb00606de1d766ef9
flipperflipper <caylaphil@gmail.com>
2021-11-30 21:28:01 +0100

    advancements made on the website

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const app = {
 
     init : () => {
         console.log('init');
-        app.drawBoard(30);
+        app.drawBoard(60);
     },
  
     drawBoard : (sizeOfBoard) => {
@@ -21,10 +21,15 @@ const app = {
                 cell.classList.add('cell');
                 row.appendChild(cell);
 
+                cell.style.width = `500 / ${sizeOfBoard}px`;
+                cell.style.height = `500 / ${sizeOfBoard}px`;
+
                 cell.addEventListener('mouseover', function() { 
                     cell.classList.add('black');
                 });
+
             }
+
 
         }
     }
